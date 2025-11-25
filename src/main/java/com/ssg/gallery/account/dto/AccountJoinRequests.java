62c0a557c4976ffcd0c00b555df1289f94c797be
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-11-25 16:25:50 +0900

    주석 추가

--- src/main/java/com/ssg/gallery/account/dto/AccountJoinRequests.java
+++ src/main/java/com/ssg/gallery/account/dto/AccountJoinRequests.java
@@ -3,7 +3,7 @@ package com.ssg.gallery.account.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-// 회원가입 요청 시 사용하는 DTO
+// 회원가입 요청 시 사용하는 DTO -> 이 DTO에 저장되는 데이터는 포론트의 회원가입 폼에서 입력된 내용
 @Getter
 @Setter
 public class AccountJoinRequests {
