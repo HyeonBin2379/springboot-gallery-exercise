b2ff6e361f3343755020fe5029f4c57bed1d2653
Hyeonbin2379 <hyeonbin2379@gmail.com>
2025-12-13 16:01:21 +0900

    스프링 시큐리티 실습3 완료

--- src/main/java/com/ssg/gallery/common/config/MySecurityConfig.java
+++ src/main/java/com/ssg/gallery/common/config/MySecurityConfig.java
@@ -2,10 +2,13 @@ package com.ssg.gallery.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -19,5 +22,16 @@ public class MySecurityConfig {
                 .authorities("ROLE_USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/v1/api/**").permitAll()
+                .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        SecurityFilterChain chain = http.build();
+        chain.getFilters().forEach(System.out::println);
+        return chain;
     }
 }
