3d9499e4c1a98fa2d14dc4926825e36abf26119d
Hyeonbin2379 <hyeonbin2379@gmail.com>
2025-12-14 23:12:12 +0900

    스프링 시큐리티 실습4 - 토큰 방식 로그인 설정 추가

--- src/main/java/com/ssg/gallery/common/config/MySecurityConfig.java
+++ src/main/java/com/ssg/gallery/common/config/MySecurityConfig.java
@@ -1,14 +1,21 @@
 package com.ssg.gallery.common.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -26,12 +33,35 @@ public class MySecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/v1/api/**").permitAll()
-                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // 가장 중요: REST API에서는 무상태성을 보장해야 하므로, 세션 관리 정책을 Stateless로 설정한다.
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests
+                    .requestMatchers("/v1/api/**").permitAll()
+                    .anyRequest().authenticated());
         SecurityFilterChain chain = http.build();
         chain.getFilters().forEach(System.out::println);
         return chain;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // localhost:5173에서 오는 GET, POST, PUT, DELETE 요청을 컨트롤러에서 처리 가능하도록 CORS 관련 설정을 변경
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Vue URL
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        // 모든 요청 URL에 관해 위의 설정을 적용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
